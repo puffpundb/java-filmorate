27aca5fdff13f279ed0326f9b621050e86bbbb38
Danil Shirykalov <shirds@yandex.ru>
2025-10-06 14:37:28 +0500

    Поправлен кодстайл

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service.film;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
-import java.util.Set;
 
 public interface FilmService {
 	void liked(Long filmId, Long userId);
