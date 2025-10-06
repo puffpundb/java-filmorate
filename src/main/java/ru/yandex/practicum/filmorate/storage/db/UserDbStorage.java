507b2f28e3218a5887ce10365e08f7eed67261f6
Danil Shirykalov <shirds@yandex.ru>
2025-10-06 14:40:27 +0500

    Поправлен кодстайл

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/UserDbStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage.db;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
